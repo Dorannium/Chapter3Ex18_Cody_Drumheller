5b9ef1c6351a72b3b139f93805d6acbba6481572
Dorannium <dorianshuu@gmail.com>
2024-10-25 10:40:04 -0400

    Add project files.

---
+++ Chapter3Ex18.cpp
@@ -0,0 +1,51 @@
+/*
+Title: Chapter 3 Exercise 18 -Interest Earned
+File Name: Chapter3Ex18
+Programmer: Cody Drumheller
+Date:20241025
+Requirements:
+Write a program that ask for the principle, the interest rate,
+and the number of times the interest is compounded.
+*/
+
+#include <iostream>
+#include <cmath>  // For pow() function
+#include <iomanip>  // For setting precision
+
+using namespace std;
+
+int main() {
+ 
+    double principal, rate, interestEarned, amountInSavings;
+    int compoundsPerYear;
+
+    cout << "Enter the principal amount: ";
+    cin >> principal;
+
+    cout << "Enter the interest rate: ";
+    cin >> rate;
+    rate /= 100;
+
+    cout << "You entered interest rate: " << rate * 100 << "%" << endl;
+
+    cout << "Enter the number of times interest is compounded per year: ";
+    cin >> compoundsPerYear;
+
+    cout << "You entered: " << compoundsPerYear << " times compounded per year." << endl;
+ 
+    amountInSavings = principal * pow((1 + rate / compoundsPerYear), compoundsPerYear);
+
+    interestEarned = amountInSavings - principal;
+
+    cout << fixed << setprecision(2);
+    cout << "Interest Rate: " << rate * 100 << "%" << endl;
+    cout << "Times Compounded: " << compoundsPerYear << endl;
+    cout << "Principal: $" << principal << endl;
+    cout << "Interest: $" << interestEarned << endl;
+    cout << "Amount in Savings: $" << amountInSavings << endl;
+
+    return 0;
+}
+
+
+
